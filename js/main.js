f15afd02ead7010e6e7932d63450d6daa153ae8b
Sergiu Costache <costache.sergiu@gmail.com>
2019-01-08 02:39:47 +0200

    set cookie

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
 	theme_chk.on('change', function () {
 		//console.log($(this).is(':checked'));
 
-		localStorage.setItem("theme", $(this).is(':checked') ? 'style_night':'style');
+		//localStorage.setItem("theme", $(this).is(':checked') ? 'style_night':'style');
+		$.cookie('theme', $(this).is(':checked') ? 'style_night':'style', { domain: '.equihub.pro' });
 
 		setTimeout(function () {
 			window.location.href = window.location.href;
